8c16e2ae45199903ba066ed7cff118edebac05ef
Labrock <vladimirpeshkov5@yandex.ru>
2025-04-15 12:49:00 +0300

    change published_at

--- src/Traits/ShouldHumanPublishDate.php
+++ src/Traits/ShouldHumanPublishDate.php
@@ -4,16 +4,23 @@ namespace GIS\TraitsHelpers\Traits;
 
 trait ShouldHumanPublishDate
 {
-    public function getPublishedHumanAttribute()
+    public function getPublishedMoscowAttribute()
     {
         $value = $this->published_at;
+        if (empty($value)) return $value;
+        return date_helper()->changeTz($value);
+    }
+
+    public function getPublishedHumanAttribute()
+    {
+        $value = $this->published_moscow;
         if (empty($value)) return $value;
         return date_helper()->format($value);
     }
 
     public function getPublishedDateAttribute(): string
     {
-        $value = $this->published_at;
+        $value = $this->published_moscow;
         if (empty($value)) return $value;
         return date_helper()->format($value, "d.m.Y");
     }
